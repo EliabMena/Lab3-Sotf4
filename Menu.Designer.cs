7386e58e20aa7579423e6acf1327b2ff65ec61a0
EDMM-978 <101439326+EliabMena@users.noreply.github.com>
2024-10-08 19:29:18 -0500

    Initial commit

---
+++ Menu.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab3_Sotf4
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.problema1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.problema2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.problema3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.problema1ToolStripMenuItem,
+            this.problema2ToolStripMenuItem,
+            this.problema3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // problema1ToolStripMenuItem
+            // 
+            this.problema1ToolStripMenuItem.Name = "problema1ToolStripMenuItem";
+            this.problema1ToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.problema1ToolStripMenuItem.Text = "Problema1";
+            // 
+            // problema2ToolStripMenuItem
+            // 
+            this.problema2ToolStripMenuItem.Name = "problema2ToolStripMenuItem";
+            this.problema2ToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.problema2ToolStripMenuItem.Text = "Problema2";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // problema3ToolStripMenuItem
+            // 
+            this.problema3ToolStripMenuItem.Name = "problema3ToolStripMenuItem";
+            this.problema3ToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.problema3ToolStripMenuItem.Text = "Problema3";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem problema1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem problema2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem problema3ToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
