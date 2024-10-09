f6ac9dafef662feaefc044295b78fea82b810551
EDMM-978 <101439326+EliabMena@users.noreply.github.com>
2024-10-08 19:56:03 -0500

    ...

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -34,6 +34,7 @@
             this.problema2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.problema3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -72,11 +73,21 @@
             this.problema3ToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
             this.problema3ToolStripMenuItem.Text = "Problema3";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
@@ -96,5 +107,6 @@
         private System.Windows.Forms.ToolStripMenuItem problema2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem problema3ToolStripMenuItem;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
     }
 }
